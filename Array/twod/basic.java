fcded433d1210550db728d0ce369590888bb8a40
Apil Poudel <appealpoudel05@gmail.com>
2025-06-12 11:31:18 +0545

    2d Array

---
+++ Array/twod/basic.java
@@ -0,0 +1,44 @@
+package Array.twod;
+
+import java.util.*;
+
+public class basic {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter Rows and Columns");
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+
+        int[][] number = new int[rows][columns];
+
+        System.out.println("Enter the All the Data");
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < columns; j++) {
+
+                number[i][j] = sc.nextInt();
+                System.out.print(" \t");
+
+            }
+            System.out.println(2);
+
+        }
+        // outpit
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < columns; j++) {
+
+                System.out.print(number[i][j] + "\t");
+                ;
+
+            }
+            System.err.println();
+
+        }
+    }
+}
