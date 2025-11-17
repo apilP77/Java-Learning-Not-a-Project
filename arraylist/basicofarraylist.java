bc806354b4e0ad018850027f3b26907b4293e4a1
Apil Poudel <appealpoudel05@gmail.com>
2025-11-17 14:55:20 +0545

    arraylsit

---
+++ arraylist/basicofarraylist.java
@@ -0,0 +1,43 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.*;
+
+class basicOfArraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+        // add
+        list.add(0);
+        list.add(100);
+        list.add(-12);
+
+        System.out.println(list);
+
+        // get
+
+        System.out.println(list.get(2));
+
+        // add a element in between
+        list.add(0, 100000);
+        System.out.println(list);
+
+        // set element
+        list.set(0, 5);
+        System.out.println(list);
+
+        // remove element
+        list.remove(3);
+        System.out.println(list);
+
+        // size
+        System.out.println(list.size());
+
+        // collections / sorts
+
+        Collection.sorts(list);
+        System.out.println(list);
+    }
+}
